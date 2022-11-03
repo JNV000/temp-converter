193ab71e1956a8fcd388b5cd225be667257724db
Jacob Vielweber <jnvielweber@gmail.com>
2022-11-02 20:50:20 -0500

    start callback test

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,15 +1,20 @@
 import PropTypes from "prop-types";
+import capitalize from "lodash/capitalize";
 
-export default function Input({ label, id }) {
+export default function Input({ label, id, handleChange }) {
   return (
     <div>
-      <label htmlFor={id}>{label}</label>
+      <label htmlFor={id} onChange={handleChange}>
+        {label || capitalize(id)}
+      </label>
       <input id={id} type="number" />
     </div>
   );
 }
+// TODO: hmwrk write a function in regular javascript to capitialize first letter only (give detailed comments) in Gist
 
 Input.propTypes = {
   label: PropTypes.string,
-  id: PropTypes.string,
+  id: PropTypes.string.isRequired,
+  handleChange: PropTypes.func.isRequired,
 };
--- src/components/Input.test.jsx
+++ src/components/Input.test.jsx
@@ -1,4 +1,6 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { vi } from "vitest";
 import Input from "./Input";
 
 it("renders an input with the proper lave and id when provided both", () => {
@@ -7,4 +9,24 @@ it("renders an input with the proper lave and id when provided both", () => {
   const input = screen.getByLabelText("Test");
 
   expect(input).toBeInTheDocument();
+});
+
+it("renders an input with proper label and id using default for the label", () => {
+  render(<Input id="test" />);
+
+  const input = screen.getByLabelText("Test");
+
+  expect(input).toBeInTheDocument();
+});
+
+it("calls a callback whenever we type in the input", () => {
+  const callback = vi.fn();
+  const user = userEvent.setup();
+
+  render(<Input id="test" handleChange={callback} />);
+
+  const input = screen.getByLabelText("Test");
+  user.type(input, "123");
+
+  expect(callback).toHaveBeenCalled();
 });
