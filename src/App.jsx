d72272ac35c5b90d397eadb6dcab1ebfdd47036f
Jacob Vielweber <jnvielweber@gmail.com>
2022-11-05 23:48:07 -0500

    Set up app html

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import Input from "./components/Input";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white">
+      <main className="bg-gray-100 p-8 rounded shadow">
+        <h1 className="text-3xl font-bold underline">Temperature converter</h1>
+        <div className="flex gap-x-4 justify-center">
+          <Input id="celsius" />
+          <Input id="celsius" />
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
