e74738eb008d35c1739309423e3255c31081ae11
Jacob Vielweber <jnvielweber@gmail.com>
2022-11-06 01:08:13 -0500

    Create reducer and pass tests

--- src/hooks/reducer.js
+++ src/hooks/reducer.js
@@ -1,5 +1,28 @@
 import { tryConvert } from "../utils/tryConvert";
 
 export default function reducer(state, action) {
-  throw new Error("Not Implemented");
+  // make new state (eslint does not like declaring in a switch)
+  const newTemps = { celsius: state.celsius, fahrenheit: state.fahrenheit }; // copy the state
+  // if Fahrenheit input was changed
+  switch (action.type) {
+    // if fahrenheit input is changed
+    case "SET_CELSIUS":
+      // use try convert function
+      newTemps.celsius = tryConvert(state.fahrenheit, "convertToCelsius");
+      // return new celsius
+      return {
+        celsius: newTemps.celsius.toString(),
+        fahrenheit: newTemps.fahrenheit.toString(),
+      };
+    // if Celsius input was changed
+    case "SET_FAHRENHEIT":
+      newTemps.fahrenheit = tryConvert(state.celsius, "convertToFahrenheit");
+
+      return {
+        celsius: newTemps.celsius.toString(),
+        fahrenheit: newTemps.fahrenheit.toString(),
+      };
+    default:
+      throw new Error("Invalid action");
+  }
 }
