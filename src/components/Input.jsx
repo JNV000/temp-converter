25b4d98d720ac27716ff615f3ca85ccc003d4d94
Jacob Vielweber <jnvielweber@gmail.com>
2022-11-05 23:20:47 -0500

    Add function to capitalize label with js

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,15 +1,25 @@
 import PropTypes from "prop-types";
-import capitalize from "lodash/capitalize";
+// import capitalize from "lodash/capitalize";
+
+function capitalizeLabel(toCapitalize) {
+  // we access the first char in the string and capitalize it.
+  const part1 = toCapitalize[0].toUpperCase();
+  // we get the rest of the string by getting a substring starting at index 1.
+  const part2 = toCapitalize.slice(1);
+  // we append them together and return the result.
+  return part1 + part2;
+  // this could have been done in 1 line
+  // return toCapitalize[0].toUpperCase() + toCapitalize.slice(1);
+}
 
 export default function Input({ label, id, handleChange }) {
   return (
     <div>
-      <label htmlFor={id}>{label || capitalize(id)}</label>
+      <label htmlFor={id}>{label || capitalizeLabel(id)}</label>
       <input type="number" id={id} onChange={handleChange} />
     </div>
   );
 }
-// TODO: hmwrk write a function in regular javascript to capitialize first letter only (give detailed comments) in Gist
 
 Input.propTypes = {
   label: PropTypes.string,
