29ce235ef8fbcfa5a256ca8a97c1018bb3193411
Jacob Vielweber <jnvielweber@gmail.com>
2022-11-06 20:55:14 -0600

    Restore previous useTemp set up

--- src/hooks/useTemp.js
+++ src/hooks/useTemp.js
@@ -3,12 +3,10 @@ import reducer from "./reducer";
 
 export default () => {
   // state should include values for both f and c temps.
-  const [state, dispatch] = useReducer(reducer, {
+  const [temps, dispatch] = useReducer(reducer, {
     celsius: 100,
     fahrenheit: 212,
   });
 
-  const { celsius, fahrenheit } = state;
-
-  return { celsius, fahrenheit, dispatch };
+  return { temps, dispatch };
 };
